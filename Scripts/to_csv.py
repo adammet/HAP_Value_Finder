5a60b8803030c8172c34277fe3c09306a7247616
Metwally <Adam.Metwally@wsp.com>
2019-09-19 14:39:48 -0700

    Fixing branches

--- Scripts/to_csv.py
+++ Scripts/to_csv.py
@@ -4,23 +4,18 @@
 #Date: 2019.09.12
 
 #Import nessecary libraries
-import camelot
+import tabula, pandas, numpy, distro
 from tkinter import messagebox
-from PyPDF2 import PdfFileReader
+
 
 #Function
 def to_csv(pdf_path, output_path):
     pdf_path = pdf_path
     output_path = output_path
-
     try:
-        tables = camelot.read_pdf(pdf_path, pages='1-2') #Get the tables
-        tables.export(output_path, f='csv',compress = True )
-
+        tabula.convert_into(pdf_path,pages='all', guess=False,output_format="CSV",output_path=output_path) #Try to convert the file
     except AttributeError:
         messagebox.showerror("Operation cancelled!", "In order to continue, please re-run the program and select a PDF file.")
 
-pdf_path = "C:/Users/CAAM070972/Desktop/Python/Test_File/Floor_6_REV03.pdf"
-output_path = pdf_path.replace(".pdf",".csv")
-to_csv(pdf_path,output_path)
+
  
